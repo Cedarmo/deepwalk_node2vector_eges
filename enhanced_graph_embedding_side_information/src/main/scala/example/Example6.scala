925f05b05c891bcca17e4d8e6540d6d3121adfd3
cedarmo <yqisong1992@163.com>
2020-04-14 19:40:04 +0800

    将deepwalk、node2vector和阿里的文章:Billion-scale Commodity Embedding for E-commerce Recommendation in Alibaba用代码实现

---
+++ enhanced_graph_embedding_side_information/src/main/scala/example/Example6.scala
@@ -0,0 +1,21 @@
+package example
+
+import breeze.linalg.DenseVector
+import sparkapplication.BaseSparkLocal
+
+object Example6 extends BaseSparkLocal {
+  def main(args:Array[String]):Unit = {
+    val spark = this.basicSpark
+    import spark.implicits._
+
+    val a = "-0.30614856@0.16460776@0.19909532@-0.48152933@-0.7925998"
+    val aVector = new DenseVector[Double](a.split("@").map(_.toDouble))
+    val b = "-0.7356615@0.6408239@-0.4380877@-1.920213@-2.628376"
+    val bVector = new DenseVector[Double](b.split("@").map(_.toDouble))
+    val similar = aVector.t * bVector
+    val eHx = math.exp(math.max(math.min(-similar, 35.0), -35.0)).toFloat
+    val probability = 1.toFloat / (1.toFloat + eHx)
+    println(probability)
+
+  }
+}
